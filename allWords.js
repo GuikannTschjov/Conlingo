06705043361ed41dbb012072d4642d9ec225ff7e
GuikannTschjov <108460607+GuikannTschjov@users.noreply.github.com>
2022-08-29 20:40:24 +0800

    js4pics

---
+++ allWords.js
@@ -0,0 +1,11 @@
+var allWords = [
+{ "eng":"mother", 
+  "txt":"⅄ṡʎż" , 
+  "pic":"https://www.emojiall.com/images/svg/twitter/1f469-200d-1f37c.svg" }, 
+{ "eng":"father", "txt":"Yṡʌż" , "url":"https://www.emojiall.com/images/svg/twitter/1f468-200d-1f37c.svg" }, 
+{ "eng":"cat", "txt":"ᑫżʌżu" , "url":"https://www.emojiall.com/images/svg/twitter/1f408.svg" },
+{ "eng":"mouse", "txt":"⅄iᴚi" , "url":"https://www.emojiall.com/images/svg/twitter/1f401.svg" },
+{ "eng":"cloth", "txt":"Īyṡ" , "url":"https://www.emojiall.com/images/svg/twitter/1f455.svg" },
+{ "eng":"flower", "txt":"Ynżbż" , "url":"https://www.emojiall.com/images/svg/twitter/1f33c.svg" },
+{ "eng":"fire", "txt":"Yṣ" , "url":"https://www.emojiall.com/images/svg/twitter/1f525.svg" }
+    ]
